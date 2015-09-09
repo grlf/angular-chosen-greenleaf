04129eaaf415d14fc0661584a2925852a66271a9
John Hoopes <john.z.hoopes@gmail.com>
2015-09-09 15:18:40 -0500

    updating angular module to allow for create mode on the chosen library

--- angular-chosen.js
+++ angular-chosen.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * Use this directive to convert drop downs into chosen drop downs.
  * http://harvesthq.github.io/chosen/
  * http://adityasharat.github.io/angular-chosen/
@@ -74,6 +74,35 @@
         max_selected_options: maxSelection,
         disable_search_threshold: searchThreshold,
         search_contains: true,
+        create_option: function(term) {
+          var chosen = this;
+          var dd_model = chosen.form_field_jq.data('add-option');
+          var data_url = chosen.form_field_jq.data('add-url');
+          var url = api_prefix + 'dd';
+          if(data_url != null){
+              url = data_url;
+          }
+
+          // check to see if there's a releates to data object
+          var relatesToSelector = chosen.form_field_jq.data('relates-to');
+          if(relatesToSelector != null){ // post with relatedid
+
+            $.post(url, {name: term, relatedId: $(relatesToSelector).val(), _token: $('meta[name="_token"]').attr('content'), model: dd_model}, function(data){
+              chosen.append_option({
+                value: data.id,
+                text: data.name
+              });
+            });
+
+          }else{ // otherwise post as normal
+            $.post(url, {name: term, _token: $('meta[name="_token"]').attr('content'), model: dd_model}, function(data){
+              chosen.append_option({
+                value: data.id,
+                text: data.name
+              });
+            });
+          }
+        },
         allow_single_deselect: allowSingleDeselect
       });
 
